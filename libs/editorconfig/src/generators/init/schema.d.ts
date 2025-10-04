1705afdb03f7c4d17910a23a8c19e40403bdcd56
Davide Di Criscito <davidedc@outlook.com>
2025-10-04 13:59:06 +0200

    fix: remove overwrite option

--- libs/editorconfig/src/generators/init/schema.d.ts
+++ libs/editorconfig/src/generators/init/schema.d.ts
@@ -2,9 +2,9 @@ export interface InitGeneratorSchema {
   /** The name of the generator or project context (used in templates) */
   name: string;
 
-  /** Whether to overwrite an existing .editorconfig file if it exists */
-  overwrite?: boolean;
-
   /** Optional subdirectory inside the workspace where the .editorconfig will be generated */
   directory?: string;
+
+  /** Simulate generation without writing files */
+  dryRun?: boolean;
 }
