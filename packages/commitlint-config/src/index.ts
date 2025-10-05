1f5cd9884e7bf4248dc776e8ee7a62f07d81db81
Davide Di Criscito <davidedc@outlook.com>
2025-10-05 18:30:07 +0200

    fix: enforce subject case to be lower-case

--- packages/commitlint-config/src/index.ts
+++ packages/commitlint-config/src/index.ts
@@ -22,7 +22,7 @@ const config: UserConfig = {
     ],
     'scope-case': [2, 'always', 'kebab-case'],
     'subject-empty': [2, 'never'],
-    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-case': [2, 'always', 'lower-case'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 72],
     'body-max-line-length': [2, 'always', 100],
